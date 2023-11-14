c60358438d92196e878954251a0c870afd8b03a2
Zied ABID <zied.abid@jobteaser.com>
2023-11-14 17:12:58 +0100

    fix(dev/openid_connect): upgrade to fix ssl issue

--- lib/openid_connect.rb
+++ lib/openid_connect.rb
@@ -67,6 +67,12 @@ module OpenIDConnect
     _http_client_ = HTTPClient.new(
       agent_name: "OpenIDConnect (#{VERSION})"
     )
+
+    # taken from v1.4.2 https://github.com/nov/openid_connect/blob/v1.4.2/lib/openid_connect.rb
+    # NOTE: httpclient gem seems stopped maintaining root certtificate set, use OS default.
+    _http_client_.ssl_config.clear_cert_store
+    _http_client_.ssl_config.cert_store.set_default_paths
+
     _http_client_.request_filter << Debugger::RequestFilter.new if debugging?
     http_config.try(:call, _http_client_)
     _http_client_
